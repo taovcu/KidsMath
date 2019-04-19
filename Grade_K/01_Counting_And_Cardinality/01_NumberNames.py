b78fda7acf81de8dec08ee000ce62f9980a6bb8c
Tao Lu <taolu@marvell.com>
2019-04-19 17:05:09 -0400

    Add Know number names test
    Signed-off-by: Tao Lu <taolu@marvell.com>

---
+++ Grade_K/01_Counting_And_Cardinality/01_NumberNames.py
@@ -0,0 +1,31 @@
+# pip install inflect
+# pip install word2number
+
+import sys
+import inflect
+from word2number import w2n
+
+
+def Number2Name(a):
+    print ('Spell {} in Characters:'.format(a))
+    line = sys.stdin.readline()[:-1]
+    p = inflect.engine()
+    if line == p.number_to_words(a):
+        print ('Correct')
+    else:
+        print ('Wrong')
+
+def Name2Number(a):
+    print ('Spell {} in number:'.format(a))
+    ans = int(sys.stdin.readline()[:-1])
+    if ans == w2n.word_to_num(a):
+        print ('Correct')
+    else:
+        print ('Wrong')
+
+# Test cases
+def KnowNumberNames():
+    for i in range(3):
+        Number2Name(i)
+
+    Name2Number('seven')
