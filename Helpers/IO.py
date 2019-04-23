3bf6d88e7c3f8aaf2951cafdab22ea2df7b1bd86
Tao Lu <taolu@marvell.com>
2019-04-23 10:50:45 -0400

    Add speech recognition features
    Signed-off-by: Tao Lu <taolu@marvell.com>

--- Helpers/IO.py
+++ Helpers/IO.py
@@ -12,15 +12,24 @@ def readChar():
     return line[0]
 
 def readInt():
-    line = sys.stdin.readline()[:-1]
-    while not line.isdigit() or line == "":
-        print("input is not an int")
-        line = sys.stdin.readline()[:-1]
-    return int(line)
+    #line = sys.stdin.readline()[:-1]
+    printTTS("Please try input the number with you voice:")
+    line = TTS.stt()
+    print(line)
+    if not line['error']:
+        t = line['text']
+    else:
+        printTTS("Please try input the number from the key board:")
+        t = sys.stdin.readline()[:-1]
+
+    while not t.isdigit() or line == "":
+        printTTS("input is not an int, input again")
+        t = sys.stdin.readline()[:-1]
+    return int(t)
 
 def printTTS(t):
     print(t)
 
     #Todo: 'Windows'/'Darwin' TTS
     if platform.system() == 'Linux':
-        TTS.speak(t)
+        TTS.tts(t)
--- Helpers/text2speech.py
+++ Helpers/text2speech.py
@@ -1,10 +1,47 @@
-import pyttsx3
+# pip3 install pyttsx3
+# pip3 install SpeechRecognition
+# apt-get install portaudio19-dev
+# pip3 install --upgrade pyaudio
 
+
+import pyttsx3
+import speech_recognition as sr
+
+# text to speech
 engine = pyttsx3.init('espeak')
 engine.setProperty('rate', 150)
 engine.setProperty('voice', 'english-us')
 
-def speak(t):
+# speech to text
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+
+def tts(t):
     engine.say(t)
     engine.runAndWait()
-    
+
+def stt():
+    # set up the response object
+    response = {
+        "error": None,
+        "text": None
+    }
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = r.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
