5582468c8ec7ec071a00926d0157e9475b7bb4c3
Tao Lu <taovcu@gmail.com>
2019-04-24 19:46:26 -0400

    Add setup for Linux
    Signed-off-by: Tao Lu <taovcu@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,14 @@
+# dependencies for Linux
+sudo apt install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install pyttsx3
+sudo pip3 install SpeechRecognition
+sudo apt-get install portaudio19-dev
+sudo pip3 install --upgrade pyaudio
+sudo pip3 install matplotlib
+sudo apt-get install python3-tk
+sudo pip3 install emoji
+sudo pip3 install inflect
+sudo pip3 install word2number
+
+
