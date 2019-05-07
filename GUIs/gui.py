62149a687b57fdc6bfcba24f8801d737ba18ad80
Tao Lu <taolu@marvell.com>
2019-05-07 18:04:52 -0400

    Add check button
    Signed-off-by: Tao Lu <taolu@marvell.com>

--- GUIs/gui.py
+++ GUIs/gui.py
@@ -1,17 +1,19 @@
 import tkinter as tk
 from tkinter import ttk 
+from tkinter import IntVar
 from tkinter import *
 
 
+
 class NotebookDemo(Frame):
- 
+    ansPanel = None
     def __init__(self, isapp=True, name='notebookdemo'):
         Frame.__init__(self, name=name)
         self.pack(expand=Y, fill=BOTH)
         self.master.title('Notebook Demo')
         self.isapp = isapp
         self._create_widgets()
- 
+
     def _create_widgets(self):
         self._create_demo_panel()
         self._create_answer_panel()
@@ -33,15 +35,20 @@ class NotebookDemo(Frame):
         self._create_descrip_tab(nb)
         self._create_text_tab(nb, "Hint")
 
+    def helloCallBack(self):
+        return; 
+
     def _create_answer_panel(self):
-        ansPanel = Frame(self, name='answer')
-        ansPanel.pack(side=LEFT, fill=BOTH, expand=Y)
+        self.ansPanel = Frame(self, name='answer')
+        self.ansPanel.pack(side=LEFT, fill=BOTH, expand=Y)
+        B = Button(self.ansPanel, text = "Check Answer", command = self.helloCallBack)
+        B.pack(side=TOP)
 
-        # create the notebook
-        nb = ttk.Notebook(ansPanel, name='ansnotebook')
-
-        nb.pack(fill=BOTH, expand=Y, padx=2, pady=3)
-        self._create_text_tab(nb, "Check Answer")
+    def _create_answer_button(self, ansFrame, ansText, CheckVar):
+        C = Checkbutton(ansFrame, text = ansText, variable = CheckVar, \
+                 onvalue = 1, offvalue = 0, height=5, width = 10)
+        C.pack(side=LEFT)
+        return C
 
  
     def _create_descrip_tab(self, nb):
@@ -49,16 +56,7 @@ class NotebookDemo(Frame):
         frame = Frame(nb, name='descrip')
  
         # widgets to be displayed on 'Description' tab
-        msg = [
-            "Ttk is the new Tk themed widget set. One of the widgets ",
-            "it includes is the notebook widget, which provides a set ",
-            "of tabs that allow the selection of a group of panels, ",
-            "each with distinct content. They are a feature of many ",
-            "modern user interfaces. Not only can the tabs be selected ",
-            "with the mouse, but they can also be switched between ",
-            "using Ctrl+Tab when the notebook page heading itself is ",
-            "selected. Note that the second tab is disabled, and cannot "
-            "be selected.\n", "Message 2"]
+        msg = ["dniakmndamndoadmnkasndkasdnkasdnkaeswdnkadnkasndkasndkasndkasdnkadnkasdnkadnaskn"]
  
         lbl = Label(frame, wraplength='4i', justify=LEFT, anchor=N,
                         text=''.join(msg))
@@ -71,23 +69,13 @@ class NotebookDemo(Frame):
         # add to notebook (underline = index for short-cut character)
         nb.add(frame, text='Question', underline=0, padding=2)
  
-    def _say_neat(self, v):
-        v.set('Yeah, I know...')
-        self.update()
-        self.after(500, v.set(''))
- 
-    # =============================================================================
-    def _create_disabled_tab(self, nb):
-        # Populate the second pane. Note that the content doesn't really matter
-        frame = Frame(nb)
-        nb.add(frame, text='Disabled', state='disabled')
  
     # =============================================================================
     def _create_text_tab(self, nb, s):
         # populate the third frame with a text widget
         frame = Frame(nb)
  
-        txt = Text(frame, wrap=WORD, width=100, height=50)
+        txt = Text(frame, wrap=WORD, width=60, height=40)
         vscroll = Scrollbar(frame, orient=VERTICAL, command=txt.yview)
         txt['yscroll'] = vscroll.set
         vscroll.pack(side=RIGHT, fill=Y)
