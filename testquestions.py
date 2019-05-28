487a7c5239d35bc6370ff904c15915921aadf95b
Tao Lu <taolu@marvell.com>
2019-05-28 10:55:14 -0400

    Add more test questions
    Signed-off-by: Tao Lu <taolu@marvell.com>

--- testquestions.py
+++ testquestions.py
@@ -3,31 +3,41 @@ testcases = {
                     [{'questiontext':'', 'expectedAns':'', 'ansList':['','','','']}],
                 'AddObjectTestCases':
                     [
-                    {'questiontext': 'Bob has 3 pears, Joy has 4 pears, how many pears do they have in total?', 'expectedAns': '7', 'ansList': ['1', '2', '4', '7'], 'pictures':[['BobSmall'], ['pear']*3, ['JoySmall'], ['pear']*4]},
-                    {'questiontext': 'Bob has 6 apples, Joy has 7 apples, how many apples do they have in total?', 'expectedAns':'13', 'ansList':['11', '13', '15', '21'], 'pictures':[['BobSmall'], ['red-apple']*6, ['JoySmall'], ['red-apple']*7]}
+                    {'questiontext': 'Bob has 3 pears, Joy has 4 pears, how many pears do they have in total?', 'expectedAns': '7', 'ansList': ['1', '2', '4', '7'], 'pictures':[['BobMedium'] + ['pear']*3, ['JoyMedium'] + ['pear']*4]},
+                    {'questiontext': 'Bob has 6 apples, Joy has 7 apples, how many apples do they have in total?', 'expectedAns':'13', 'ansList':['11', '13', '15', '21'], 'pictures':[['BobMedium'] + ['red-apple']*6, ['JoyMedium'] + ['red-apple']*7]}
                     ],
                 'AddTestCases':
                     [{'questiontext':'Add numbers \n 4 + 5 = ?', 'expectedAns':'9', 'ansList':['6', '9', '12', '13']},
-                     {'questiontext':'Add numbers \n 5 + 8 = ?', 'expectedAns':'13', 'ansList':['6', '9', '12', '13']}
+                     {'questiontext':'Add numbers \n 5 + 8 = ?', 'expectedAns':'13', 'ansList':['6', '9', '12', '13']},
+                     {'questiontext':'Add numbers \n 9 + 9 = ?', 'expectedAns':'18', 'ansList':['6', '9', '12', '18']}
                     ],
                 'Number2NameCases':
                     [{'questiontext':'Translate Digit to English words \n\n 7', 'expectedAns':'Seven', 'ansList':['Five', 'Seven', 'Nine', 'Eleven']},
-                     {'questiontext':'Translate Digit to English words \n\n 11', 'expectedAns':'Eleven', 'ansList':['Five', 'Seven', 'Nine', 'Eleven']}
+                     {'questiontext':'Translate Digit to English words \n\n 11', 'expectedAns':'Eleven', 'ansList':['Five', 'Seven', 'Nine', 'Eleven']},
+                     {'questiontext':'Translate Digit to English words \n\n 18', 'expectedAns':'Eighteen', 'ansList':['Five', 'Eighteen', 'Nine', 'Eleven']}
                     ],
                 'Name2NumberCases':
-                    [{'questiontext':'Translate English words to digit \n\n Eight', 'expectedAns':'8', 'ansList':['3', '5', '8', '11']}],
+                    [{'questiontext':'Translate English words to digit \n\n Eight', 'expectedAns':'8', 'ansList':['3', '5', '8', '11']},
+                     {'questiontext':'Translate English words to digit \n\n Thirteen', 'expectedAns':'8', 'ansList':['3', '19', '13', '11']},
+                     {'questiontext':'Translate English words to digit \n\n Nineteen', 'expectedAns':'8', 'ansList':['13', '19', '8', '11']}
+                    ],
                 'countNumby10sCases':
                     [{'questiontext':'Count number by tens in sequence \n\n Count from 10 to 100 by tens', 'expectedAns':'10 20 30 40 50 60 70 80 90 100', 'ansList':['10 20 30 40 50 60 70 80 90 100', '10 20 30 40 60 70  80  90 100', '10 20 30  50  60 70 80 90 100', '20  30  40 50 60 70 80 90 100']}],
                 'countNumfromXCases':
                     [{'questiontext':'Count number from 6 to 13', 'expectedAns':'6 7 8 9 10 11 12 13', 'ansList':['6 7  9 10 11 12 13', '6  7  8 9 10 11 13', '7  8 9 10  11 12 13', '6 7 8 9 10 11 12 13']}],
                 'writeNumCases':
-                    [{'questiontext':'Write number 8 in English word', 'expectedAns':'Eight', 'ansList':['Seven', 'Eight', 'Nine', 'Ten']}],
+                    [{'questiontext':'Write number 8 in English word', 'expectedAns':'Eight', 'ansList':['Seven', 'Eight', 'Nine', 'Ten']},
+                    {'questiontext':'Write number 13 in English word', 'expectedAns':'Thirteen', 'ansList':['Seven', 'Thirteen', 'Nine', 'Ten']}
+                    ],
                 'countObjectCases':
-                    [{'questiontext':'How many pears do you see?', 'expectedAns':'6', 'ansList':['5', '6', '7', '8'], 'pictures':[['pear']*6]}],
+                    [{'questiontext':'How many pears do you see?', 'expectedAns':'6', 'ansList':['5', '6', '7', '8'], 'pictures':[['pear']*6]},
+                     {'questiontext':'How many apples do you see?', 'expectedAns':'13', 'ansList':['5', '16', '13', '8'], 'pictures':[['red-apple']*13]}
+                    ],
                 'compareObjectCases':
-                    [{'questiontext':'Who has more pears, Joy or Bob?', 'expectedAns':'Joy', 'ansList':['Joy', 'Bob', '', ''], 'pictures':[['JoySmall'], ['pear']*8, ['BobSmall'], ['pear']*6]}],
+                    [{'questiontext':'Who has more grapes, Joy or Bob?', 'expectedAns':'Joy', 'ansList':['Joy', 'Bob'], 'pictures':[['JoyMedium'] + ['grapes']*8, ['BobMedium'] + ['grapes']*6]}],
                 'compareNumCases':
-                    [{'questiontext':'Choose the greatest number', 'expectedAns':'12', 'ansList':['6', '9', '12', '10']}],
+                    [{'questiontext':'Choose the greatest number', 'expectedAns':'12', 'ansList':['6', '9', '12', '10']},
+                    {'questiontext':'Choose the greatest number', 'expectedAns':'18', 'ansList':['6', '9', '12', '18']}],
                 'subtractTestCases':
                     [
                         {'questiontext':'Subtract numbers \n\n 10 - 5 = ?', 'expectedAns':'5', 'ansList':['6', '5', '12', '13']},
